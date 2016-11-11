c595e90b2a9963bd42635306d35aef04c243fe54
jpmolden <john.paul.molden@gmail.com>
2016-11-10 20:44:26 -0800

    Update lab4_code.c

--- Lab4/lab4_code.c
+++ Lab4/lab4_code.c
@@ -707,7 +707,7 @@ void check_ADCs(){
 	ADCSRA |= (1<<ADIF); //its done, clear flag by writing a one 
 
 	adc_result = ADCL; //read the ADC output as 16 bits
-	OCR2 = 0x30; //adc_result;
+	OCR2 = adc_result; //adc_result;
 //return(adc_data);
 	
 }
