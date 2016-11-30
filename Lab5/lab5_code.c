60fdcf018d21590818c7fda8d02404130c08ac28
jpmolden <john.paul.molden@gmail.com>
2016-11-29 20:05:32 -0800

    Update lab5_code.c

--- Lab5/lab5_code.c
+++ Lab5/lab5_code.c
@@ -94,7 +94,9 @@ uint8_t alarm_msg[16] = {'A', 'L', 'A', 'R', 'M', ' ', ' ', ' ', ' ', ' ', ' ', 
 	char temp_text[16] = "In:   C Out:   C";
 	//char lcd_display[32] = lcd_line1 + lcd_line2;
 	char lcd_display[32];
-	char lcd_string_array[16]; //holds a string to refresh the LCD
+	char lcd_string_array[32]; //holds a string to refresh the LCD
+	lcd_string_array = {'A', 'L', 'A', 'R', 'M', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
+			   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', ' ', ' ', ' ', ' ', ' '};
 	uint16_t lm73_temp; //a place to assemble the temperature from the lm73
 
 
