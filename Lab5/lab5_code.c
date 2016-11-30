27c3af5b9c6577450684862982466c147f5132f9
jpmolden <john.paul.molden@gmail.com>
2016-11-29 20:01:23 -0800

    Update lab5_code.c

--- Lab5/lab5_code.c
+++ Lab5/lab5_code.c
@@ -861,8 +861,9 @@ ISR(TIMER0_OVF_vect){
 	lm73_temp |= lm73_rd_buf[1];  //"OR" in the low temp byte to lm73_temp 
 	lm73_temp = lm73_temp >> 7;
 	
-	itoa(lm73_temp, lcd_string_array, 10); //convert to string in array with itoa() from avr-libc                           
-	string2lcd(lcd_string_array); //send the string to LCD (lcd_functions)
+	refresh_lcd(lcd_string_array);
+	//itoa(lm73_temp, lcd_string_array, 10); //convert to string in array with itoa() from avr-libc                           
+	//string2lcd(lcd_string_array); //send the string to LCD (lcd_functions)
 	
 	
 
