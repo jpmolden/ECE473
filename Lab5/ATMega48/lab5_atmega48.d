203b5dea01fe34466678c03f97cfb7f55f5423d7
JPM <john.paul.molden@gmail.com>
2016-11-30 22:08:15 -0800

    a

---
+++ Lab5/ATMega48/lab5_atmega48.d
@@ -0,0 +1,2 @@
+lab5_atmega48.o lab5_atmega48.d : lab5_atmega48.c mega48_uart_functions.h \
+ lm73_functions_skel.h twi_master.h
---
+++ Lab5/ATMega48/mega48_uart_functions.d
@@ -0,0 +1,1 @@
+mega48_uart_functions.o mega48_uart_functions.d : mega48_uart_functions.c
