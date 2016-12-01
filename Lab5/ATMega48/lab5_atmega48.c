9d7d78d7511cc8276b8cc36d1580c0066e23a11e
jpmolden <john.paul.molden@gmail.com>
2016-11-30 22:48:21 -0800

    Update lab5_atmega48.c

--- Lab5/ATMega48/lab5_atmega48.c
+++ Lab5/ATMega48/lab5_atmega48.c
@@ -4,6 +4,7 @@
 // 10.26.16
 
 
+// Includes
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -12,20 +13,21 @@
 #include "lm73_functions_skel.h"
 #include "twi_master.h"
 
-                                                char temperature[2] = {'3', '5'};
-uint8_t rx_m128_command;
+//Defines
+#define TRUE 1
+#define FALSE 0
+
+
+//External Variable
 extern uint8_t lm73_wr_buf[2];
 extern uint8_t lm73_rd_buf[2];
-uint16_t lm73_temp;
-char lm73_char_temp[8];
 
+//Variable Defns
+uint8_t rx_m128_command;
+uint16_t lm73_temp; //a place to assemble the temperature from the lm73
+uint8_t new_data_needed
+char temp_string_array[3] = {' ', ' ', ' '};
 
-
-//
-
-
-
-//
 
 int main() {
 
@@ -33,19 +35,39 @@ int main() {
     uart_init();
     sei();
 
+    
+  
+    //Initial Temparature Data
     lm73_wr_buf[0] = 0x00;
     twi_start_wr(LM73_ADDRESS, lm73_wr_buf, 2);
 
+    lm73_temp = (lm73_rd_buf[0] << 8) | (lm73_rd_buf[1]);
+    lm73_temp = lm73_temp >> 7;
+    //Populate the local temparature data
+    itoa(lm73_temp, temp_string_array, 10);
+  
+  
+  
     while(1) {
-        twi_start_rd(LM73_ADDRESS, lm73_rd_buf, 2);
-        _delay_ms(2);
-        //format temperature array
-        lm73_temp = (lm73_rd_buf[0] << 8) | (lm73_rd_buf[1]);
-        lm73_temp = lm73_temp >> 7;
-        itoa(lm73_temp, lm73_char_temp, 10);
-        _delay_ms(500);
-    }
-    
+        
+      //Read the temp over TWI
+
+      
+      //Get new data once the USART Intterupt Sends
+      if(new_data_needed == TRUE){
+          //Read the temp over TWI
+          twi_start_rd(LM73_ADDRESS, lm73_rd_buf, 2);
+          _delay_ms(2);
+          lm73_temp = (lm73_rd_buf[0] << 8) | (lm73_rd_buf[1]);
+          lm73_temp = lm73_temp >> 7;
+          //Populate the local temparature data
+          itoa(lm73_temp, temp_string_array, 10);
+          new_data_needed = FALSE;
+      }//End if
+      
+    //Do Nothing unless new data needed or interrupt
+      
+    }//End While
 }//main
 
 
@@ -58,7 +80,8 @@ ISR(USART_RX_vect) {
     rx_m128_command = uart_getc();
     if(rx_m128_command == 0xAB)
         //If the recieved command is valid send the temparature
-        uart_puts(lm73_char_temp);
+        uart_puts(temp_string_array);
+        new_data_needed = TRUE;
 }
 
 
